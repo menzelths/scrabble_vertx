e6467fb50c8bd7db412d6c1c8ee1df6a6d3e4b7a
thomas <thomas@192.168.0.102>
2014-06-12 20:26:23 +0200

    alle wesentlichen regeln implementiert, allerdings noch leichte probleme beim verbinden

--- src/main/resources/web/spieler.js
+++ src/main/resources/web/spieler.js
@@ -25,18 +25,21 @@ $(document).ready(function(){
         this.wert=wert;
     }
     
-    function intitialisiereSpielfeld(){
+    function initialisiereSpielfeld(){
         $("body").html("Spieler Nummer "+spielernummer+"<br>");
         
         $("body").append("<canvas width='"+breite+"px' height='"+hoehe+"px' id='bild'/>");
-        $("body").append("<div id='aktuelleIdee'/>");
-        $("body").append("<div id='OK'></div>");
-        $("body").append("<div id='loeschen'>Neue Idee</div>");
+        $("body").append("<div class='idee' id='aktuelleIdee'/><br>");
         
+        $("body").append("<span class='knopf' id='loeschen'>Neue Idee</span><p>");
+        $("body").append("<div id='bistdran'><span class='knopf' id='tauschen' >Tauschen</span><span class='knopf' id='passen' >Passen</span><span class='knopf' id='OK' >Wort ok</span></div><br>");
         $("body").append("<div id='bisherigeIdeen' />");
         $("body").append("<div id='nachricht'></div>");
+        $("body").append("<div id='navigation'></div>");
         if (geradedran===true){
-            $("#OK").text("OK");
+            $("#bistdran").show();
+        } else {
+            $("#bistdran").hide();
         }
         c=$("#bild")[0].getContext("2d");
         $("#bild").mousedown(function(event) {
@@ -90,10 +93,15 @@ $(document).ready(function(){
         selektoren=[];
         aktuellewahl=[];
         $("#aktuelleIdee").html(aktuelleIdee);
-       
+        $("#navigation").html("");
         belegt=[];
         for (var i=0;i<aktuelleBuchstaben.length;i++){
             zeichneBuchstabe(i,"#000000");
+        }
+        if (geradedran===true){
+            $("#bistdran").show();
+        } else {
+            $("#bistdran").hide();
         }
     });
     $("#OK").click(function(){
@@ -105,7 +113,28 @@ $(document).ready(function(){
                 text+=$("#"+aktuellewahl[i]).val();; // aktuelle option auslesen
             }
         }
-        $("#bisherigeIdeen").html(text);
+        //$("#bisherigeIdeen").html(text);
+        eb.send("scrabble.spielfeld",{typ:"vorschlag",wort:text,nr:uuid});
+        
+            $("#bistdran").hide();
+        
+    });
+    $("#passen").click(function(){ // zug aussetzen
+        eb.send("scrabble.spielfeld",{typ:"passen",nr:uuid},function(antwort){
+        });
+    });
+    $("#tauschen").click(function(){
+        var text="";
+        for (var i=0;i<aktuellewahl.length;i++){
+            if (aktuellewahl[i]!=="?"){
+            if (aktuellewahl[i].length===1){
+                text+=aktuellewahl[i];
+            } else {
+                text+=_;
+            }
+        }
+        }
+        eb.send("scrabble.spielfeld",{typ:"tausch",wert:text});
     });
     }
     
@@ -126,7 +155,7 @@ $(document).ready(function(){
     }
     
     function zeichneSpielfeld(){
-        intitialisiereSpielfeld();
+        initialisiereSpielfeld();
         
         c.fillStyle="#008800";
         c.fillRect(0,0,breite,hoehe);
@@ -139,7 +168,7 @@ $(document).ready(function(){
     
     eb.onopen = function() {
 
-      eb.registerHandler('scrabble.spieler.'+uuid, function(message) { // eigene adresse
+      eb.registerHandler('scrabble.spieler.'+uuid, function(message,replier) { // eigene adresse
 
         var typ=message.typ;
         if (typ==="nr"){
@@ -149,6 +178,7 @@ $(document).ready(function(){
                 $("body").append("<br><input type='button' id='start' value='Starte das Spiel'></input>");
                 $("#start").click(function(){
                     eb.send("scrabble.spielfeld",{typ:"start"});
+                    //replier({typ:"start"});
                 });
             }
         } else if (typ==="ablehnung"){
@@ -164,19 +194,93 @@ $(document).ready(function(){
             buchstabenwerte=message.werte;
             zeichneSpielfeld();
         } else if (typ==="bistdran"){
-            $("#OK").text("OK");
-            $("#nachricht").html("Du bist dran!");
+            $("#bistdran").show();
+            //$("#nachricht").html("Du bist dran!");
             geradedran=true;
+            belegt=[];
+            aktuellewahl=[];
+            zeichneSpielfeld();
+        } else if (typ==="bistnichtdran"){
+            $("#bistdran").hide();
+            //$("#nachricht").html("Du bist NICHT dran!");
+            // belegte steine weg
+            var sammel=[];
+            for (var i=0;i<aktuelleBuchstaben.length;i++){
+                if (belegt.indexOf(i)===-1){
+                    sammel.push(aktuelleBuchstaben[i]);
+                }
+            }
+            aktuelleBuchstaben=[];
+            for (var i=0;i<sammel.length;i++){
+            aktuelleBuchstaben[i]=sammel[i];
         }
+            var neu=message.wert;
+            for (var i=0;i<neu.length;i+=2){
+                aktuelleBuchstaben.push(new Buchstabe(neu[i],neu[i+1]));
+            
+        }
+            geradedran=false;
+            belegt=[];
+            zeichneSpielfeld();
+        } else if (typ==="verschieben"){
+            $("#navigation").html("<div class='wrapper'><span class='navi  schnellknopf' dy='-3' dx='0'>3 HOCH</span></div><br>");
+            $("#navigation").append("<div class='wrapper'><span class='navi knopf' dy='-1' dx='0'>HOCH</span></div><p>");
+            $("#navigation").append("<div class='wrapper-links'><span class='navi schnellknopf' dy='0' dx='-3' >3 LINKS</span></div>");
+            $("#navigation").append("<div class='wrapper-rechts'><span class='navi schnellknopf' dy='0' dx='3'>3 RECHTS</span></div>");
+            $("#navigation").append("<div class='wrapper-links'><span class='navi knopf' dy='0' dx='-1' >LINKS</span></div>");
+            $("#navigation").append("<div class='wrapper-rechts'><span class='navi knopf' dy='0' dx='1'  >RECHTS</span></div><p>");
+            $("#navigation").append("<div class='wrapper'><span class='navi knopf' dy='1' dx='0'  >RUNTER</span></div><br>");
+            $("#navigation").append("<div class='wrapper'><span class='navi  schnellknopf' dy='3' dx='0'  >3 RUNTER</span></div><p>");
+            $("#navigation").append("<div class='wrapper'><span id='rotieren' class='knopf'  >DREHEN</span></div><p>");
+            $("#navigation").append("<span id='okwahl' class='knopf' >OK</span>");
+            $(".navi").click(function(){
+                 
+           
+                eb.send("scrabble.spielfeld",{typ:"schieben",dx:$(this).attr("dx"),dy:$(this).attr("dy"),nr:uuid,drehen:false});
+            });
+            $("#rotieren").click(function(){
+                eb.send("scrabble.spielfeld",{typ:"schieben",dx:0,dy:0,nr:uuid,drehen:true});
+            });
+            $("#okwahl").click(function(){
+               eb.send("scrabble.spielfeld",{typ:"wort_ok",nr:uuid});
+            });
+            
+        } else if (typ==="warten"){
+            $("body").html("Bitte warten! Deine Wörter werden soeben von den anderen überprüft!");
+            
+            
+        } else if (typ==="ueberpruefung"){
+            $("body").html("Gibt es deiner Ansicht nach ALLE folgenden Wörter?<p>");
+            var daten=message.werte;
+            for (var i=0;i<daten.length;i++){
+                $("body").append("<br>"+daten[i]);
+            }
+            $("body").append("<p><input type='button' value='JA' id='jaknopf'/><p>");
+            $("body").append("<p><input type='button' value='NEIN' id='neinknopf'/><p>");
+            $("#jaknopf").click(function(){
+                eb.send("scrabble.spielfeld",{typ:"ja",nr:uuid});
+                zeichneSpielfeld();
+            });
+            $("#neinknopf").click(function(){
+                eb.send("scrabble.spielfeld",{typ:"nein",nr:uuid});
+                zeichneSpielfeld();
+            });
+        } else if (typ==="ok_da"){
+            $("#okwahl").show();
+        } else if (typ==="ok_weg"){
+            $("#okwahl").hide();
+        } 
 
       });
       
-            eb.registerHandler('scrabble.alle', function(message) { // nachricht von allen
+            eb.registerHandler('scrabble.alle', function(message,replier) { // nachricht von allen
         var typ=message.typ;
         if (typ==="anmeldung"){
             $("#nachricht").html("Spieler Nummer "+message.wert+" wurde angemeldet");
         } else if (typ==="start"){
             $("body").html("Es geht gleich los ...");
+        } else if (typ==="weiter"){
+            zeichneSpielfeld();
         }
       });
       
