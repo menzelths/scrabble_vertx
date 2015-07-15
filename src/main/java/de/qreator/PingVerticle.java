e59056cd0846b9473bd014f9af8b8ea2f4bbf865
Thomas Menzel <menzelths@yahoo.de>
2015-07-15 23:03:10 +0200

    Umstellung auf vertx 3.0 und alle javascript-dateien lokal in fatjar

--- src/main/java/de/qreator/PingVerticle.java
+++ src/main/java/de/qreator/PingVerticle.java
@@ -18,21 +18,61 @@ package de.qreator;
  */
 
 
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import java.net.InetAddress;
 
-import org.vertx.java.core.http.HttpServer;
+/*import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
-
+*/
 /*
  This is a simple Java verticle which receives `ping` messages on the event bus and sends back `pong` replies
  */
-public class PingVerticle extends Verticle {
+public class PingVerticle  {
 
-    @Override
-    public void start() {
+   
+    public static void main(String [] s) {
+        int port=8080;
+        if (s.length==1){
+            port=Integer.parseInt(s[0]); // betriebsmodus festlegen: 
+            
+            
+        }
+        
+        Vertx vertx = Vertx.vertx();
+        io.vertx.core.http.HttpServer server = vertx.createHttpServer();
+        
+        Router router = Router.router(vertx); 
+        SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+        PermittedOptions [] inboundPermitted=new PermittedOptions[3];
+        inboundPermitted[0] = new PermittedOptions().setAddress("scrabble.alle");
+        inboundPermitted[1] = new PermittedOptions().setAddress("scrabble.spielfeld");
+        inboundPermitted[2] = new PermittedOptions().setAddressRegex("scrabble.spieler\\..+");
+        //PermittedOptions outboundPermitted = new PermittedOptions().setAddress("de.qreator.led");
+        BridgeOptions options = new BridgeOptions();
+        for (int i=0;i<3;i++){
+            options.addInboundPermitted(inboundPermitted[i]);
+            options.addOutboundPermitted(inboundPermitted[i]);
+        }
+        
 
+        sockJSHandler.bridge(options);
+        
+        router.route("/bridge/*").handler(sockJSHandler);
+        router.route("/*").handler(StaticHandler.create()); // webroot unter src/main/resources/webroot        
+        server.requestHandler(router::accept).listen(port);
+
+        EventBus eb = vertx.eventBus();
+        
+        
+/*
         HttpServer server = vertx.createHttpServer();
         server.requestHandler(req -> {
             String file = req.path().equals("/") ? "index.html" : req.path();
@@ -60,11 +100,13 @@ public class PingVerticle extends Verticle {
         vertx.createSockJSServer(server).bridge(config, inboundPermitted, outboundPermitted);
 
         server.listen(8080);
+        */
         try{
-        System.out.println("Spieler bitte mit Browser anmelden unter \nhttp://"+InetAddress.getLocalHost().getHostAddress()+":8080/spieler.html");
+        System.out.println("Spieler bitte mit Browser anmelden unter \nhttp://"+InetAddress.getLocalHost().getHostAddress()+":"+port+"/spieler.html");
         
         } catch(Exception e){
             e.printStackTrace();
         }
+        
     }
 }
